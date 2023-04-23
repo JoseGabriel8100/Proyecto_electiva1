6657535f6f2bd9a708342cbf52c9a7e185d3ca43
JoseGabriel8100 <115415377+JoseGabriel8100@users.noreply.github.com>
2023-04-23 01:55:35 -0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,24 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('xyz')
+  getXyz(): string {
+    return 'Hello World!';
+  }
+
+  @Get('abc/:param')
+  getParam(@Param('param') param: string): string {
+    return `Param received: ${param}`;
+  }
+
+  @Get('pqr')
+  getQueryParams(@Query() queryParams: any): string {
+    const { param1, param2 } = queryParams;
+    return `Params received: ${param1} and ${param2}`;
   }
 }
